4dd541312e894e31afb717ece7e8f17a46a9505b
pthyde123 <pth7@cornell.edu>
2024-12-18 16:16:44 -0500

    save

--- scripts/pea_available_vs_need.R
+++ scripts/pea_available_vs_need.R
@@ -26,8 +26,8 @@ final_data_count1 %>%
   left_join(pea_seed_available, by = join_by(accession == accession)) %>% # join seed needs(cynthia) with seed have (garrett)
   
   mutate(seed_per_plot = (5 * 60)) %>%     #5 seeds/sqft for pea (60% of full rate, 8 seeds/sqft) # rate from email
-  mutate(seed_per_gram = (1000/`TSW(g)`)) %>%  #calcualte the seed per grams for each accession 
-  mutate(g_pea_per_plot = (seed_per_plot / seed_per_gram)/GERM_RESULTS) %>% # cacluate the seed grams of seed for each accessions to get the desired seeding rate, including seed size and germination rate
+  mutate(seed_per_gram = (1000/`TSW(g)`)) %>%  #calculate the seed per grams for each accession 
+  mutate(g_pea_per_plot = (seed_per_plot / seed_per_gram)/GERM_RESULTS) %>% # calculate the seed grams of seed for each accessions to get the desired seeding rate, including seed size and germination rate
   mutate(g_pea_per_accession = g_pea_per_plot * Count) %>% # calculate the total amount of seed needed per accession 
   
   mutate(extra_seed = (pea_g_available - g_pea_per_accession)) %>% #how much seed do we have minus how much seed do we need

a77c6e805c3286554a832ea21f996be8e16e8937
pthyde123 <pth7@cornell.edu>
2024-12-23 13:31:09 -0500

    updated the seed need seed available calculations, took need strait from randomization

--- scripts/oat_needed_per_accession.R
+++ scripts/oat_needed_per_accession.R
@@ -4,21 +4,25 @@ library(tidyverse)
 library(readxl)
 
 
-
-final_data_count1 <- read_csv("data/final_data_count1.csv")
-
+update_pea_oat_experiment_design <- read_csv("data/update_pea_oat_experiment_design.csv")
 
 
-final_data_count1 %>% 
-  filter(crop == "Oat") %>%  # just looking at oat accessions
+
+update_pea_oat_experiment_design %>% 
+  select(!combo) %>% #remove old combo
+  group_by(Oat) %>% 
+  count() %>%  # count the number of plots needed per accession
+  rename(accession = Oat) %>% 
+  rename(plot_count = n) %>% 
   
   mutate(g_oat_per_plot = 24) %>% # Juan's quote from email
+  mutate(oat_g_available = 480) %>% 
   
-  mutate(g_oat_per_accession = g_oat_per_plot * Count) %>% # calculate the total amount of seed needed per accession 
-  
-  arrange(g_oat_per_accession) %>% 
-  select(accession,Count,g_oat_per_accession) %>% 
-  print(n= nrow(final_data_count1))
+  mutate(g_oat_per_accession = g_oat_per_plot * plot_count) %>% # calculate the total amount of seed needed per accession 
+  mutate(extra_oat_seed = (oat_g_available - g_oat_per_accession)) %>% 
+    
+  arrange(extra_oat_seed) %>% 
+  print(n= nrow(update_pea_oat_experiment_design))
 
 
 
--- scripts/pea_available_vs_need.R
+++ scripts/pea_available_vs_need.R
@@ -1,4 +1,3 @@
-
 library(readr)
 library(tidyverse)
 library(readxl)
@@ -8,38 +7,35 @@ library(readxl)
 ### pea seed list from Garrett
 B4I_FINAL_pea_seed_list <- read_excel("data/B4I_FINAL_pea_seed_list.xlsx")
 
-
-### number of plots needed per accession from Cynthia 
-##final_data_count1 <- read_csv("data/final_data_count1.csv")
-
-final_data_count1 <- Seed_distribution <- read_csv("data/Seed_distribution.csv")
-
 pea_seed_available <- B4I_FINAL_pea_seed_list %>% 
-  mutate(pea_g_available = as.numeric(if_else(`SEED_AVAILABLE(g)` ==">1000","1500",`SEED_AVAILABLE(g)`))) %>%  
+  mutate(pea_g_available = as.numeric(if_else(`SEED_AVAILABLE(g)` ==">1000","1500",`SEED_AVAILABLE(g)`))) %>%  # change >1000 to 1500(the grams sent)
   select(NAME,pea_g_available,`TSW(g)`,GERM_RESULTS) %>% 
   rename(accession = NAME)
 
 
+### randomization from Cynthia
+update_pea_oat_experiment_design <- read_csv("data/update_pea_oat_experiment_design.csv")
 
-final_data_count1 %>% 
-  filter(crop == "Pea") %>%  # just looking at Pea accessions
+
+### calculating pea need per accession from randomization
+update_pea_oat_experiment_design %>% 
+  select(!combo) %>% #remove old combo
+  group_by(Pea) %>% 
+  count() %>%  # count the number of plots needed per accession
+  rename(accession = Pea) %>% 
+  rename(plot_count = n) %>% 
+  
   left_join(pea_seed_available, by = join_by(accession == accession)) %>% # join seed needs(cynthia) with seed have (garrett)
   
   mutate(seed_per_plot = (5 * 60)) %>%     #5 seeds/sqft for pea (60% of full rate, 8 seeds/sqft) # rate from email
   mutate(seed_per_gram = (1000/`TSW(g)`)) %>%  #calculate the seed per grams for each accession 
   mutate(g_pea_per_plot = (seed_per_plot / seed_per_gram)/GERM_RESULTS) %>% # calculate the seed grams of seed for each accessions to get the desired seeding rate, including seed size and germination rate
-  mutate(g_pea_per_accession = g_pea_per_plot * Count) %>% # calculate the total amount of seed needed per accession 
+  mutate(g_pea_per_accession = g_pea_per_plot * plot_count) %>% # calculate the total amount of seed needed per accession 
   
   mutate(extra_seed = (pea_g_available - g_pea_per_accession)) %>% #how much seed do we have minus how much seed do we need
   
   arrange(extra_seed) %>% 
-  select(accession,Count,extra_seed) %>% 
-  print(n= nrow(final_data_count1))
-
-  
-
-
-
+  print(n= nrow(update_pea_oat_experiment_design))
 
 
 
